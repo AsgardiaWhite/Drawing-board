b9212a62e4533bd59c15df56e8e1c0c03573f79e
lily <lily@lily.local>
2016-11-17 23:20:11 +0800

    Initial Commit

---
+++ DrawPrj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawPrj
+//
+//  Created by lily on 16/11/17.
+//  Copyright © 2016年 lily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawPrj/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DrawPrj
+//
+//  Created by lily on 16/11/17.
+//  Copyright © 2016年 lily. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
