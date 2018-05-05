3712b0c24b37c553b2d75160d30b71b112d07235
Anderson White <839693523@qq.com>
2018-05-04 20:49:36 -0700

    1.0版
    添加手势解锁和绘画板的功能

--- DrawPrj/ViewController.m
+++ DrawPrj/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "UnBlockView.h"
+#import "CanvasViewController.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UnBlockView *blockView = [[UnBlockView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:blockView];
+    
+    blockView.unblockViewBlock = ^(UnBlockView *bView, BOOL success) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"title" message:success?@"解锁成功":@"解锁失败" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [bView reset];
+            if (success) {
+                //跳转到一个新的页面
+                CanvasViewController *vc = [[CanvasViewController alloc] init];
+                UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+                [nav setNavigationBarHidden:YES];
+                [self presentViewController:nav animated:YES completion:nil];
+            }
+        }];
+        [alert addAction:action];
+        [self presentViewController:alert animated:YES completion:nil];
+    };
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
